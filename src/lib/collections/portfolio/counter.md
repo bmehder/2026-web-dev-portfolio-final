ce8a83f54dfe1d5c6c624331287e797f3d6f9733
bmehder <bmehder@gmail.com>
2026-04-21 22:29:38 +0100

    Counter

---
+++ src/lib/collections/portfolio/counter.md
@@ -0,0 +1,17 @@
+---
+  title: Counter Component
+  date: 2026-04-21
+  excerpt: This is the classic Reactive User Interface (UI) example using a Svelte component.
+---
+
+<script>
+  import Counter from "$lib/components/Counter.svelte"
+</script>
+
+## Something
+
+<p>This is some html.</p>
+
+<Counter />
+
+<p>This is some html too.</p>
---
+++ src/lib/components/Counter.svelte
@@ -0,0 +1,30 @@
+<script>
+	let count = $state(0)
+
+  const decrement = () => count = count - 1
+  const increment = () => count = count + 1
+</script>
+
+<div class="counter flex align-items-center">
+  <button onclick={decrement}>-</button>
+	<span>{count}</span>
+  <button onclick={increment}>+</button>
+</div>
+
+<style>
+  button {
+    background-color: var(--color-pink);
+    color: var(--color-white);
+    border: none;
+  }
+
+  button:focus-visible {
+    outline-offset: 3px;
+  }
+
+  span {
+    min-width: 6ch;
+    font-family: monospace;
+    text-align: center;
+  }
+</style>
