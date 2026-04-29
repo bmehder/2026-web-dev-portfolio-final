63480de7303857137587e8e96d3bd53592d33b02
bmehder <bmehder@gmail.com>
2026-04-29 12:24:00 +0100

    MobileHeader

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -16,6 +16,7 @@
       </nav>
     </div>
   </div>
+  
 </header>
 
 <style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,16 +3,25 @@
 	import favicon from '$lib/assets/favicon.svg'
 	import Footer from '$lib/components/Footer.svelte'
 	import Header from '$lib/components/Header.svelte'
+	import MobileHeader from '$lib/components/MobileHeader.svelte'
+	import MobileHeaderTest from '$lib/components/MobileHeaderTest.svelte'
 
 	let { children } = $props()
+	let innerWidth = $state()
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<svelte:window bind:innerWidth />
+
 <div class="page-wrapper">
-	<Header></Header>
+	{#if innerWidth > 768}
+		<Header></Header>
+	{:else}
+		<MobileHeaderTest></MobileHeaderTest>
+	{/if}
 	<main>{@render children()}</main>
 	<Footer></Footer>
 </div>
