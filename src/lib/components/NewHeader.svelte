853e50eeb57accf4edf0d83d816888201abcf74f
bmehder <bmehder@gmail.com>
2026-04-29 17:02:03 +0100

    NewHeader

---
+++ src/lib/components/NewHeader.svelte
@@ -0,0 +1,110 @@
+<script>
+	import DarkLogo from '$lib/assets/logo-dark.svg'
+	import Hamburger from '$lib/assets/icons/hamburger.svg'
+	import Close from '$lib/assets/icons/close.svg'
+
+	let isOpen = $state(false)
+
+	const closeNav = () => (isOpen = false)
+</script>
+
+<header>
+	<div class="outer">
+		<div class="inner">
+			<div class="container">
+				<a href="/" class="logo">
+					<img src={DarkLogo} alt="logo" />
+				</a>
+
+				<button
+					aria-label="Toggle navigation"
+					aria-expanded={isOpen}
+					aria-controls="site-nav"
+					onclick={() => (isOpen = !isOpen)}
+				>
+					{#if isOpen}
+						<img src={Close} alt="close icon" />
+					{:else}
+						<img src={Hamburger} alt="open icon" />
+					{/if}
+				</button>
+
+				<nav id="site-nav" class={{ open: isOpen }}>
+					<ul>
+						<li><a href="/" onclick={closeNav}>Home</a></li>
+						<li><a href="/about" onclick={closeNav}>About</a></li>
+						<li>
+							<a href="/portfolio" onclick={closeNav}>Portfolio</a>
+						</li>
+						<li>
+							<a class="button bg-primary light" href="/contact" onclick={closeNav}
+								>Contact</a
+							>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</div>
+</header>
+
+<style>
+	.inner {
+		padding-block: 1.5rem;
+	}
+
+	.container {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	/* --- Mobile Trigger --- */
+	button {
+		width: 2rem;
+		padding: 0;
+		background: none;
+		border: none;
+		cursor: pointer;
+
+		@media (width > 48rem) {
+			display: none;
+		}
+	}
+
+	/* --- Nav --- */
+	ul {
+		display: flex;
+		align-items: center;
+		gap: 1.5rem;
+		padding: 0;
+		list-style: none;
+	}
+
+	/* --- Mobile styles --- */
+	@media (width < 48rem) {
+		.container {
+			flex-wrap: wrap;
+		}
+
+		nav {
+			min-width: 100%;
+			display: grid;
+			grid-template-rows: 0fr;
+		}
+
+		ul {
+			margin: 0;
+			flex-direction: column;
+			align-items: start;
+			overflow: hidden;
+		}
+
+		/* Open state */
+		.open {
+			min-height: 100svh;
+			grid-template-rows: 1fr;
+			margin-top: 1rem;
+		}
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,18 @@
 <script>
 	import '$lib/style.css'
 	import favicon from '$lib/assets/favicon.svg'
+	import NewHeader from '$lib/components/NewHeader.svelte'
 	import Footer from '$lib/components/Footer.svelte'
-	import Header from '$lib/components/Header.svelte'
-	import MobileHeader from '$lib/components/MobileHeader.svelte'
-	import MobileHeaderTest from '$lib/components/MobileHeaderTest.svelte'
 
 	let { children } = $props()
-	let innerWidth = $state()
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<svelte:window bind:innerWidth />
-
 <div class="page-wrapper">
-	{#if innerWidth > 768}
-		<Header></Header>
-	{:else}
-		<MobileHeaderTest></MobileHeaderTest>
-	{/if}
+	<NewHeader></NewHeader>
 	<main>{@render children()}</main>
 	<Footer></Footer>
 </div>
