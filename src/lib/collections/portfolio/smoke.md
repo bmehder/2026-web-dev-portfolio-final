57a43c68255a27b244417a9ae8741d4c877e78a1
bmehder <bmehder@gmail.com>
2026-06-11 11:27:25 +0100

    smoke cursor portfolio item

---
+++ src/lib/collections/portfolio/smoke.md
@@ -0,0 +1,13 @@
+---
+  title: Smoke Cursor
+  date: 2026-06-11
+  excerpt: This is a trailing smoke cursor effect using a CSS animation.
+  image: placeholder-3.png
+---
+
+<script>
+  import Smoke from "$lib/components/Smoke.svelte"
+</script>
+
+<Smoke />
+
---
+++ src/lib/components/Smoke.svelte
@@ -0,0 +1,41 @@
+<script>
+	let container
+
+	const createPlume = (event) => {
+		const div = document.createElement('div')
+		div.classList.add('plume')
+		div.style.top = event.clientY + "px"
+		div.style.left = event.clientX + "px"
+		
+		container.appendChild(div)
+		
+		div.addEventListener('animationend', () => { div.remove() })
+	}
+</script>
+
+<svelte:window onmousemove={createPlume} />
+
+<div bind:this={container} id="container"></div>
+ 
+<style>
+	#container {
+		min-block-size: 100svh;
+		margin: 0;
+		/* background-color: black; */
+	}
+
+	:global(.plume) {
+		position: fixed;
+		inline-size: 10px;
+		block-size: 10px;
+		background-image: radial-gradient(circle, rebeccapurple, transparent 80%);
+		animation: billow 1s ease-in forwards;
+	}
+
+	@keyframes billow {
+		to {
+			transform: scale(10) translateY(-10px);
+			opacity: 0;
+		}
+	}
+</style>
